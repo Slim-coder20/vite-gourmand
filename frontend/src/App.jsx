0fd681d923531ab832665617fea585ae442a4cc6
slim <slimabida21@gmail.com>
2025-12-01 19:13:19 +0100

    feat(footer): Implémentation du footer avec disposition en colonnes
    - Création du composant Footer avec structure en grid
    - Ajout des horaires d'ouverture en haut avec logo aligné
    - Implémentation des réseaux sociaux avec icônes SVG (Facebook, Instagram, Twitter)
    - Ajout des liens légaux alignés avec le copyright
    - Réduction du padding pour un espacement plus équilibré
    - Ajout du système flexbox pour placer le footer en bas de page
    - Styles responsive pour mobile et tablette
    - Utilisation de modules CSS pour le styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,18 @@
 import "./index.css";
 import Header from "./components/header/Header";
 import PromoBanner from "./components/promoBanner/PromoBanner";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
-    <>
+    <div className="app-container">
       <Header />
       <PromoBanner />
-    </>
+      <main className="main-content">
+        {/* Votre contenu principal ira ici */}
+      </main>
+      <Footer />
+    </div>
   );
 }
 
